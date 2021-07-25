bba481333479263f3b42cbe721e8060341a38688
Ridwan226 <4515210063@univpancasila.ac.id>
2021-07-25 23:46:16 +0700

    Most Picked

--- front-staycation-ridwan/src/pages/LandingPages.js
+++ front-staycation-ridwan/src/pages/LandingPages.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 
 import Header from 'parts/Header';
+import MostPicked from 'parts/MostPicked';
 import landingPage from 'assets/json/landingPage';
 import Hero from 'parts/Hero';
 
 export default class LandingPages extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
     );
   }
